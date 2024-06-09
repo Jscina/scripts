bc5d9e16efe79c0a5581b4ffd5d02978dcee0699
Josh <joshscina@gmail.com>
2024-06-09 19:51:42 -0400

    Setup scriptisto and multiprocess the conversion

--- convert_to_pdf.py
+++ convert_to_pdf.py
@@ -1,30 +1,36 @@
+#!/usr/bin/env scriptisto
+
 import argparse
 import glob
 import shutil
 import subprocess
-from enum import StrEnum
-from functools import partial
+from multiprocessing import Pool, cpu_count
 from pathlib import Path
 
+# scriptisto-begin
+# script_src: convert_to_pdf.py
+# build_cmd: pyinstaller convert_to_pdf.py --onefile
+# target_bin: dist/convert_to_pdf
+# scriptisto-end
 
-class FileType(StrEnum):
-    ODT = ".odt"
-    PDF = ".pdf"
 
-def sort_chapters(file: Path, file_type: FileType) -> int:
-    return int(file.name.split()[1].strip(file_type.value))
+
+def sort_chapters(file: Path) -> int:
+    return int(file.name.split()[1].strip(".pdf"))
+
+def process_odt_files(tmp_dir: Path, file_path: Path) -> None:
+    subprocess.run(["libreoffice", "--headless", "--convert-to", "pdf", "--outdir", tmp_dir, file_path], check=True)
 
 def convert_odt_to_pdf(odt_dir: Path, output_pdf: Path) -> None:
     tmp_dir = Path.home() / "tmp"
     files = map(Path, glob.glob("**/*.odt", root_dir=odt_dir, recursive=True))
-    for file in sorted(files, key=partial(sort_chapters, file_type=FileType.ODT)):
-        file_path = odt_dir / file
-        print(f"Converting {file_path}")
-        subprocess.run(["libreoffice", "--headless", "--convert-to", "pdf", "--outdir", tmp_dir, file_path], check=True)
-    pdfs = sorted(map(Path, glob.glob(f"{tmp_dir}/*.pdf")), key=partial(sort_chapters, file_type=FileType.PDF))
+    file_paths = [odt_dir / file for file in files]
+    with Pool(cpu_count()) as pool:
+        pool.starmap(process_odt_files, [(tmp_dir, file_path) for file_path in file_paths])
+    pdfs = sorted(map(Path, glob.glob(f"{tmp_dir}/*.pdf")), key=sort_chapters)
     subprocess.run(["pdfunite", *pdfs, output_pdf], check=True)
     print(f"Output PDF: {output_pdf}")
-    shutil.rmtree(tmp_dir)
+    shutil.rmtree(tmp_dir, onexc=print)
 
 
 def main() -> None:
