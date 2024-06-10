435613e9ff0ec217200c182b83aea4268fc3fe2a
Josh <joshscina@gmail.com>
2024-06-09 20:16:04 -0400

    Multiprocessing no worky

--- convert_to_pdf.py
+++ convert_to_pdf.py
@@ -4,7 +4,6 @@ import argparse
 import glob
 import shutil
 import subprocess
-from multiprocessing import Pool, cpu_count
 from pathlib import Path
 
 # scriptisto-begin
@@ -14,23 +13,29 @@ from pathlib import Path
 # scriptisto-end
 
 
-
 def sort_chapters(file: Path) -> int:
     return int(file.name.split()[1].strip(".pdf"))
 
-def process_odt_files(tmp_dir: Path, file_path: Path) -> None:
-    subprocess.run(["libreoffice", "--headless", "--convert-to", "pdf", "--outdir", tmp_dir, file_path], check=True)
 
 def convert_odt_to_pdf(odt_dir: Path, output_pdf: Path) -> None:
     tmp_dir = Path.home() / "tmp"
     files = map(Path, glob.glob("**/*.odt", root_dir=odt_dir, recursive=True))
     file_paths = [odt_dir / file for file in files]
-    with Pool(cpu_count()) as pool:
-        pool.starmap(process_odt_files, [(tmp_dir, file_path) for file_path in file_paths])
-    pdfs = sorted(map(Path, glob.glob(f"{tmp_dir}/*.pdf")), key=sort_chapters)
-    subprocess.run(["pdfunite", *pdfs, output_pdf], check=True)
-    print(f"Output PDF: {output_pdf}")
-    shutil.rmtree(tmp_dir, onexc=print)
+    for file in file_paths:
+        try:
+            subprocess.run(["libreoffice", "--headless", "--convert-to", "pdf", "--outdir", tmp_dir, file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+            print(f"Converted {file} to PDF")
+        except Exception as e:
+            print(f"Error: {e}")
+    try:
+        pdfs = sorted(map(Path, glob.glob(f"{tmp_dir}/*.pdf")), key=sort_chapters)
+        subprocess.run(["pdfunite", *pdfs, output_pdf], check=True)
+        print(f"Output PDF: {output_pdf}")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        shutil.rmtree(tmp_dir, onexc=print)
+
 
 
 def main() -> None:
